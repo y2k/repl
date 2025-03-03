824b1fb33d16456c5c98f1ca1643c270c426a3c3
y2k <itwisterlx@gmail.com>
2025-03-03 02:25:33 +0100

    refactoring

--- test/test.clj
+++ test/test.clj
@@ -3,19 +3,22 @@
     (:require ["../nrepl/nrepl" :as nrepl]
               ["../interpreter/interpreter" :as i]))
 
-(defn ^void main [^"String[]" args]
+(defn- assert_ [expected ^String code]
   (checked!
    (let [close_server (nrepl/main (fn [e l] (i/eval e l)) 8090 (atom (i/make_env {})))
          socket (Socket.)]
      (.connect socket (InetSocketAddress. "localhost" 8090) 1000)
      (let [out (.getOutputStream socket)
            in (.getInputStream socket)
-           data (.getBytes "(\n+\n2\n2\n)")
+           data (.getBytes code)
            len_buf (ByteBuffer/allocate 4)]
        (.putInt len_buf (.-length data))
        (.write out (.array len_buf))
        (.write out data)
        (let [actual (String. (.readAllBytes in))]
          (close_server)
-         (if (not= "41" actual)
+         (if (not= expected actual)
            (FIXME actual)))))))
+
+(defn ^void main [^"String[]" _]
+  (assert_ "4" "(\n+\n2\n2\n)"))
